cd2c9fc8e811e74320f0f04bd6e552a5b2a1df59
belmo <belmo@6F2EDA7BCFFB557>
2025-08-20 23:11:04 +0200

    DataAccessLayer

---
+++ BattlefieldCompetitivePortal.Framework/Data/DatabaseHelper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattlefieldCompetitivePortal.Framework.Data
+{
+    public static class DatabaseHelper
+    {
+        private static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["BattlefieldPortal"].ConnectionString;
+
+        public static DataTable ExecuteQuery(string query, params SqlParameter[] parameters)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    conn.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+
+                }
+            }
+            return dt;
+        }
+
+        public static int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                        conn.Open();
+
+                    }
+
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static T ExecuteScalar<T>(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                        conn.Open();
+
+                    }
+
+                    object result = cmd.ExecuteScalar();
+                    return result == DBNull.Value ? default(T) : (T)result;
+                }
+            }
+        }
+    }
+}
+
+
+
+
